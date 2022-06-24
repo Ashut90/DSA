e7f3875d8b47cca50f6cc95a27e3d0353e93dd26
Ashu <ashutosh.formin@gmail.com>
2022-06-24 09:12:12 +0800

    Heap.py

---
+++ Heap.py
@@ -0,0 +1,30 @@
+# problem to find the Kth smallest element in 2D matrix
+
+from heapq import heappush, heappop
+
+
+def ksmallest(matrix, k):
+    n_rows = len(matrix)
+    n_col = len(matrix[0])
+
+    min_heap = []
+
+# travers the elements inside the matrix
+    for i in range(n_rows):
+        for j in range(n_rows):
+            heappush(min_heap, matrix[i][j])
+
+    # pop k number of times to get kth smallest element
+
+    for i in range(k):
+        result = heappop(min_heap)
+
+    return result
+
+
+matrix = [[1, 4, 7],
+          [3, 5, 9],
+          [6, 8, 11]]
+k = 5
+res = ksmallest(matrix, k)
+print("kth smallest element in 2D Matrix is : ", res)
