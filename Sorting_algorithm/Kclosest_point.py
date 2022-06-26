60887caeb49231ce414c85c57f8b385b50c87693
Ashu <ashutosh.formin@gmail.com>
2022-06-26 22:46:24 +0800

    Kclosest_point.py is a question asked in facebook interview

---
+++ Sorting_algorithm/Kclosest_point.py
@@ -0,0 +1,28 @@
+from heapq import heappop, heappush
+import math
+
+
+def kclosestpoints(points, k):
+    def get_distance(x, y):
+        return math.sqrt(x ** 2 + y ** 2)
+
+    min_heap = []
+    n = len(points)
+    for i in range(n):
+        x = points[i][0]
+        y = points[i][1]
+
+        # inserting the distance and the points inside a minheap
+        heappush(min_heap, (get_distance(x, y), points[i]))
+
+    result = []
+
+    for i in range(k):
+        result.append(heappop(min_heap)[1])
+    return result
+
+
+points = [[2, -1], [3, 2], [4, 1], [-1, -1], [-2, 2]]
+k = 3
+result = kclosestpoints(points, k)
+print("closest points to the origin are : ", result)
